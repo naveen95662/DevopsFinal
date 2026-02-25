19f41f72f14811d6e7c0bd3ef9c3c2f42c3a6ba7
naveen95662 <95662naveen@gmail.com>
2026-02-25 21:41:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('DevOps Capstone Project Running Successfully 🚀');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
